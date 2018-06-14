9cca2176ab8b22d7d02d1a9b28cddc98055a61c7
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-06-14 14:33:22 -0700

    refactor: disable service worker
    for now service worker caching will be managed by the overall layout.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 import App from './App';
 import RegisterReact from 'reactive-elements';
-import registerServiceWorker from './registerServiceWorker';
 
 RegisterReact.registerReact('communication-preferences', App);
 registerServiceWorker();
