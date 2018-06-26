28492b4b5234c15201b8c4fa92a1a8a8f0ba513f
Chris Paraiso <cparaiso@unicon.net>
2018-06-26 14:05:46 -0700

    Add oidc and response properties. wire in prepopulation for form, update proptypes

--- src/App.js
+++ src/App.js
@@ -8,27 +8,26 @@ const log = (type) => console.log.bind(console, type);
 
 class App extends Component {
     static propTypes = {
-        form: PropTypes.string
-    };
-
-    static defaultProps = {
-        form: '/default/api/url/here'
+        formUrl: PropTypes.string.isRequired,
+        responseUrl: PropTypes.string.isRequired,
+        oidcUrl: PropTypes.string
     };
 
     state = {
         schema: {},
-        uiSchema: {}
+        uiSchema: {},
+        formData: {}
     };
 
     fetchSchema = async () => {
-        const {form} = this.props;
+        const {formUrl, oidcUrl} = this.props;
 
         try {
             // open /Applications/Google\ Chrome.app --args --disable-web-security --user-data-dir
-            const response = await fetch(form, {
+            const response = await fetch(formUrl, {
                 credentials: 'same-origin',
                 headers: {
-                    'Authorization': 'Bearer ' + (await oidc({userInfoApiUrl: 'http://localhost:8080/uPortal/api/v5-1/userinfo', timeout: 18000})).encoded,
+                    'Authorization': 'Bearer ' + (oidcUrl ? (await oidc({userInfoApiUrl: oidcUrl, timeout: 18000})).encoded : (await oidc({timeout: 18000})).encoded),
                     'content-type': 'application/jwt',
                   }
             });
@@ -38,8 +37,8 @@ class App extends Component {
             }
 
             const payload = await response.json();
-            const uiSchema = payload[0].metadata;
-            const schema = payload[0].schema;
+            const uiSchema = payload.metadata;
+            const schema = payload.schema;
 
             this.setState({schema, uiSchema});
         } catch (err) {
@@ -48,12 +47,42 @@ class App extends Component {
         }
     };
 
-    componentDidMount = this.fetchSchema;
+    fetchFormData = async () => {
+        const {responseUrl, oidcUrl} = this.props;
+
+        try {
+            const response = await fetch(responseUrl, {
+                credentials: 'same-origin',
+                headers: {
+                    'Authorization': 'Bearer ' + (oidcUrl ? (await oidc({userInfoApiUrl: oidcUrl, timeout: 18000})).encoded : (await oidc({timeout: 18000})).encoded),
+                    'content-type': 'application/jwt',
+                  }
+            });
+
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+
+            const payload = await response.json();
+            const formData = payload.answers;
+            this.setState({formData});
+        } catch (err) {
+            // error
+            console.error(err);
+        }
+    };
+
+    getForm = () => {
+        this.fetchSchema();
+        this.fetchFormData();
+    };
+
+    componentDidMount = this.getForm;
 
     render = () => {
-        const {schema, uiSchema} = this.state;
+        const {schema, uiSchema, formData} = this.state;
         return (
-            <Form schema={schema} uiSchema={uiSchema} onChange={log("changed")} onSubmit={log("submitted")} onError={log("errors")} />
+            <Form schema={schema} uiSchema={uiSchema} formData={formData} onChange={log("changed")} onSubmit={log("submitted")} onError={log("errors")} />
         );
     }
 }
