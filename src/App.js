47a34f8298f6a0485612ca17f9daa2dc0393b65b
Chris Paraiso <cparaiso@unicon.net>
2018-07-06 15:35:41 -0700

    Refactor error handling and oidc calls

--- src/App.js
+++ src/App.js
@@ -27,33 +27,43 @@ class App extends Component {
         errorMessage: ''
     };
 
-    handleOidc = (err, token) => {
-        console.info('*********** handle oidc', err, token);
-        if (err) {
-            const hasError = true;
-            const errorMessage = 'There was a problem authorizing this request.';
-            this.setState({hasError, errorMessage});
-            throw new Error(err);
+    handleOidcError = (err) => {
+        console.error(err);
+        this.setState({hasError: true, errorMessage: 'There was a problem authorizing this request.'});
+    };
+
+    handleFbmsError = (err) => {
+        let message;
+
+        if (err.type === 'submission') {
+            message = 'There was a problem submitting your form.';
         } else {
-            return token.encoded;
+            message = 'There was a problem finding your form.';
+        }
+
+        this.setState({hasError: true, errorMessage: message});
+    };
+
+    getToken = async () => {
+        const {oidcUrl} = this.props;
+
+        try {
+            const {encoded} = await oidc({userInfoApiUrl: oidcUrl, timeout: 18000});
+            return encoded;
+        } catch (err) {
+            console.error(err);
+            this.handleOidcError(err);
         }
     };
 
-    handleFbmsError = () => {
-        const hasError = true;
-        const errorMessage = 'There was a problem finding your form.';
-        this.setState({hasError, errorMessage});
-    }
-
     fetchSchema = async () => {
-        const {fbmsBaseUrl, fbmsFormFname, oidcUrl} = this.props;
-
+        const {fbmsBaseUrl, fbmsFormFname} = this.props;
         try {
             // open /Applications/Google\ Chrome.app --args --disable-web-security --user-data-dir
             const response = await fetch(fbmsBaseUrl + '/api/v1/forms/' + fbmsFormFname, {
                 credentials: 'same-origin',
                 headers: {
-                    'Authorization': 'Bearer ' + (oidcUrl ? (await oidc({userInfoApiUrl: oidcUrl, timeout: 18000}, this.handleOidc)) : (await oidc({timeout: 18000}, this.handleOidc))),
+                    'Authorization': 'Bearer ' + await this.getToken(),
                     'content-type': 'application/jwt',
                 }
             });
@@ -71,25 +81,24 @@ class App extends Component {
             this.fetchFormData();
         } catch (err) {
             // error
-            console.error(err);
+            console.error('fdfdf' + err);
         }
     };
 
     fetchFormData = async () => {
-        const {fbmsBaseUrl, fbmsFormFname, oidcUrl} = this.props;
+        const {fbmsBaseUrl, fbmsFormFname} = this.props;
 
         try {
             const response = await fetch(fbmsBaseUrl + '/api/v1/submissions/' + fbmsFormFname, {
                 credentials: 'same-origin',
                 headers: {
-                    'Authorization': 'Bearer ' + (oidcUrl ? (await oidc({userInfoApiUrl: oidcUrl, timeout: 18000}, this.handleOidc)) : (await oidc({timeout: 18000}, this.handleOidc))),
+                    'Authorization': 'Bearer ' + await this.getToken(),
                     'content-type': 'application/jwt',
                 }
             });
 
             if (!response.ok) {
                 if (response.status !== 404) {
-                    this.handleFbmsError();
                     throw new Error(response.statusText);
                 } else {
                     return;
@@ -100,8 +109,8 @@ class App extends Component {
             const formData = payload.answers;
             this.setState({formData});
         } catch (err) {
-            // error
             console.error(err);
+            this.handleFbmsError(err);
         }
     };
 
@@ -117,28 +126,27 @@ class App extends Component {
     };
 
     submitForm = async (userFormData) => {
-        const {fbmsBaseUrl, fbmsFormFname, oidcUrl} = this.props;
+        const {fbmsBaseUrl, fbmsFormFname} = this.props;
         const body = this.transformBody(userFormData);
         try {
             const response = await fetch(fbmsBaseUrl + '/api/v1/submissions/' + fbmsFormFname, {
                 method: 'POST',
                 credentials: 'same-origin',
                 headers: {
-                    'Authorization': 'Bearer ' + (oidcUrl ? (await oidc({userInfoApiUrl: oidcUrl, timeout: 18000}, this.handleOidc)) : (await oidc({timeout: 18000}, this.handleOidc))),
+                    'Authorization': 'Bearer ' + await this.getToken(),
                     'content-type': 'application/json',
                 },
                 body: JSON.stringify(body)
             });
 
             if (!response.ok) {
-                    this.handleFbmsError();
-                    throw new Error(response.statusText);
+                console.info('*********** response', response);
+                throw new Error(response.statusText);
             }
-
-            const payload = await response.json();
         } catch (err) {
-            // error
+            err.type = 'submission';
             console.error(err);
+            this.handleFbmsError(err);
         }
     };
 
