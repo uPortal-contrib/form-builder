f88ccfda718af34439f0afef335747cf2e7d2b62
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-07-16 20:06:41 -0700

    refactor: update font awesome
    remove unused files project stylesheets.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import Form from "react-jsonschema-form";
 import PropTypes from 'prop-types';
 import oidc from '@uportal/open-id-connect';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faExclamationCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'regenerator-runtime/runtime';
+
+library.add(faExclamationCircle, faCheckCircle);
 
 const log = (type) => console.log.bind(console, type);
 
