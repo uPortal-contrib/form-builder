b19f6b88865703f28c4a2402e220c235da08f01b
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-02-23 15:26:25 -0700

    refactor: support create react app version 2
    create react app version 2:
    * attempts to break the bundle into multiple parts, this was avoided by
    adding a rewired build task that disables the splitting option
    * moves the proxy out of package.json and into the magic setupProxy.js
    file
    * errors on prop type typos, boolean needed to be corrected to bool

---
+++ scripts/build-non-split.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const rewire = require('rewire');
+const defaults = rewire('react-scripts/scripts/build.js');
+let config = defaults.__get__('config');
+
+config.optimization.splitChunks = {
+    cacheGroups: {
+        default: false,
+    },
+};
+
+config.optimization.runtimeChunk = false;
