a65e36c2ecaf152d596e5697ba9f06604fd6aef9
Benito J. Gonzalez <bgonzalez@unicon.net>
2025-12-12 16:47:52 -0800

    feat: initial lit impl

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-// See: https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development#configuring-the-proxy-manually
-
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy('/uPortal', { target: 'http://localhost:8080' }));
-  app.use(proxy('/fbms', { target: 'http://localhost:8080' }));
-};
