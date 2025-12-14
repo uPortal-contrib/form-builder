d6c9afd52119f4dd984d6bb64194e696a34a6b4e
Benito Gonzalez <bgonzalez@unicon.net>
2025-12-14 10:31:05 -0800

    Test/add testing (#92)
    * chore: add test tooling and initial tests
    
    * fix: correct code and tests, plus clean up

---
+++ web-test-runner.config.js
@@ -0,0 +1,46 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
+export default {
+  files: 'test/**/*.test.js',
+  nodeResolve: true,
+  
+  // Test framework
+  testFramework: {
+    config: {
+      timeout: 500,
+    },
+  },
+
+  // Browser configuration
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    // Optionally test in other browsers:
+    // playwrightLauncher({ product: 'firefox' }),
+    // playwrightLauncher({ product: 'webkit' }),
+  ],
+
+  // Coverage configuration
+  coverage: true,
+  coverageConfig: {
+    threshold: {
+      statements: 80,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+    },
+    reportDir: 'coverage',
+  },
+
+  // Test isolation
+  testRunnerHtml: testFramework =>
+    `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
+};
